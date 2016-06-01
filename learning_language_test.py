046428849afa9a1b0193ba521395655099cfc905
Geraldo Guiesi Junior <geraldo.guiesi@gmail.com>
2016-06-01 00:25:48 -0300

    remove unused files

--- learning_language_test.py
+++ learning_language_test.py
@@ -1,12 +1,6 @@
 from learning_language import MergeSubtitle
 
-
-path = '/Users/guiesi/Downloads/torrent/Penny.Dreadful.S03E04.720p.HDTV.x264-AVS[rarbg]/'
-merge_subs = MergeSubtitle(path + 'Penny.Dreadful.S03E04.720p.HDTV.x264-AVS.en.srt',
-                          path + 'Penny.Dreadful.S03E04.720p.HDTV.x264-AVS.pt.srt')
-merge_subs.merge_subtitles(path + 'teste.srt')
-
-path = '/Users/guiesi/Downloads/torrent/The.Flash.2014.S02E23.HDTV.x264-LOL[ettv]/'
-merge_subs = MergeSubtitle(path + 'the.flash.2014.223.hdtv-lol[ettv].en.srt',
-                           path + 'the.flash.2014.223.hdtv-lol[ettv].pt.srt')
-merge_subs.merge_subtitles(path + 'teste.srt')
+path = '/Users/guiesi/Downloads/Anomalisa.2015.BRRip.XviD.AC3-EVO/'
+merge_subs = MergeSubtitle(path + 'Anomalisa.2015.BRRip.XviD.AC3-EVO.en.srt',
+                           path + 'Anomalisa.2015.BRRip.XviD.AC3-EVO.pt.srt')
+merge_subs.merge_subtitles(path + 'Anomalisa.2015.BRRip.XviD.AC3-EVO..srt')
